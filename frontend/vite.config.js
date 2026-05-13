62d7d6d20e9ff0fc7a2ac115ac4bcc30602c4cb0
jhojanguti12 <jhojanguti12@gmail.com>
2026-05-13 17:39:08 -0500

    inicio 10

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,14 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    allowedHosts: ['floristeria-api-v2.onrender.com'], // Esto quita el error de pantalla negra
-    host: '0.0.0.0',
-    port: 5173
-  },
   preview: {
-    allowedHosts: ['floristeria-api-v2.onrender.com'], // También para el modo producción
+    allowedHosts: ['floristeria-api-v2.onrender.com'],
     host: '0.0.0.0',
-    port: 5173
+    port: 5173,
+    strictPort: true
   }
 })

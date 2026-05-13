3e08b5b7da55ea0badde527b3b0f9d62ab185047
jhojanguti12 <jhojanguti12@gmail.com>
2026-05-13 17:32:04 -0500

    inicio 9

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Hemos quitado la importación de tailwindcss de aquí para evitar el error de "Module Not Found"
 export default defineConfig({
-  plugins: [
-    react()
-  ],
+  plugins: [react()],
   server: {
+    allowedHosts: ['floristeria-api-v2.onrender.com'], // Esto quita el error de pantalla negra
+    host: '0.0.0.0',
+    port: 5173
+  },
+  preview: {
+    allowedHosts: ['floristeria-api-v2.onrender.com'], // También para el modo producción
     host: '0.0.0.0',
     port: 5173
   }

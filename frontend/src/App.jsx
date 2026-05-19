644faea2e38bc1c28ec4b7a9784c769c248bb3ee
jhojanguti12 <jhojanguti12@gmail.com>
2026-05-19 17:25:36 -0500

    arreglo 4

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -85,7 +85,7 @@ export default function App() {
       <div className="min-h-screen flex items-center justify-center p-4 relative overflow-hidden font-sans">
         {/* 1. Fondo de pantalla usando tu imagen local de la carpeta public */}
         <div className="absolute inset-0 z-0" style={{
-            backgroundImage: "url('/fondo-jardin.jpg')",
+            backgroundImage: "url('https://images.unsplash.com/photo-1558293842-c0ff3db86157?q=80&w=1920')",
             backgroundSize: 'cover', 
             backgroundPosition: 'center'
           }}
